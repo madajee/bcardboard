51c9eab0de6190f872d362925adcc45b1a4af47f
Jeetan Madaan <madajeeblog@gmail.com>
2024-03-31 13:38:45 -0400

    c4

--- controllers.js
+++ controllers.js
@@ -1,5 +1,5 @@
 app.controller('bcardboardController', ['$scope', '$location',
   function($scope, $location) {
-    $scope.books = [{'title': 'test1'}, {'title': 'test2'}]
+    $scope.books = [{'title': 'book1'}, {'title': 'book2'}, {'title': 'book3'}, {'title': 'book4'}]
     $scope.url = $location.path()
   }]);
